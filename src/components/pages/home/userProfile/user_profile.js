00bd0a3efb98517d00e1b27ddbf395abeda2611e
fatwaanugerah21 <fatwaanugerah21@gmail.com>
2020-11-02 12:16:29 +0800

    Perbaiki tampilan untuk Page Phone-Number

--- src/components/pages/home/userProfile/user_profile.js
+++ src/components/pages/home/userProfile/user_profile.js
@@ -1,19 +1,21 @@
-import React from 'react';
-import userProfile from '../../../../images/alAlam.jpg';
-import { Link } from 'react-router-dom';
-import './user-profile.css'
+import React from "react";
+// import userProfile from "";
+import { Link } from "react-router-dom";
+import "./user-profile.css";
 
 const UserProfile = () => {
-   return (
-      <div className="user-profil">
-         <section className="profil-avatar">
-            <img src={userProfile} alt="" />
-         </section>
-         <section className="profil-text">
-            <p className="profil-name">Fatwa anugerah nasir</p>
-            <p className="profil-email"><Link to="/">fatwaanugerah21@gmail.com</Link></p>
-         </section>
-      </div>
-   )
-}
-export default UserProfile
+  return (
+    <div className="user-profil">
+      <section className="profil-avatar">
+        <img src="/images/alAlam.jpg" alt="" />
+      </section>
+      <section className="profil-text">
+        <p className="profil-name">Fatwa anugerah nasir</p>
+        <p className="profil-email">
+          <Link to="/">fatwaanugerah21@gmail.com</Link>
+        </p>
+      </section>
+    </div>
+  );
+};
+export default UserProfile;
