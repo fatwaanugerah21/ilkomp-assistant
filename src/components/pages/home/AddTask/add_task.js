dfec9b37de459d54e80ff210377608709eadd6d9
fatwaanugerah21 <fatwaanugerah21@gmail.com>
2020-11-09 18:48:07 +0800

    fix main menu display

--- src/components/pages/home/AddTask/add_task.js
+++ src/components/pages/home/AddTask/add_task.js
@@ -1,18 +1,16 @@
-import React from 'react';
-import './add_task.min.css'
-import { Link } from 'react-router-dom'
-
+import React from "react";
+import "./add_task.min.css";
+import { Link } from "react-router-dom";
 
 const AddTask = () => {
-
    return (
       <div className="add-task-menu">
          <Link to="add-task">
             <div className="mask">
-               <h1>Tambah Tugas</h1>
+               <h1 className="menu-name">Tambah Tugas</h1>
             </div>
          </Link>
       </div>
-   )
-}
-export default AddTask
+   );
+};
+export default AddTask;
--- src/components/pages/home/TaskList/task_list.js
+++ src/components/pages/home/TaskList/task_list.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './task_list.min.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./task_list.min.css";
 
 const TaskList = () => {
    return (
       <div className="task-list-menu">
          <div className="mask">
             <Link to="/task-list">
-               <h1>Daftar Tugas</h1>
+               <h1 className="menu-name">Daftar Tugas</h1>
             </Link>
          </div>
       </div>
-   )
-}
-export default TaskList
+   );
+};
+export default TaskList;
